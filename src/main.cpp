a39a7999e9fc7e39d63e56aca5f23060b38d3255
Kei Moomin Sugano <tobasojyo@gmail.com>
2019-10-11 17:40:31 +0900

    appropriate assigning of arg value --xxx [vakue] s into hash

--- src/main.cpp
+++ src/main.cpp
@@ -15,8 +15,10 @@ using namespace std;
 bool startsWith(char *text)
 {
     int i =0;
-    while (text[i] != '\0'){
-        if(text[i-1] == '-' && text[i] == '-'){
+    while (text[i] != '\0')
+    {
+        if(text[i-1] == '-' && text[i] == '-')
+        {
             return true;
         }
         i++;
@@ -27,12 +29,20 @@ bool startsWith(char *text)
 int main(int argc, char *argv[])
 {
     map<string, string> argm;
+    string              value;
 
     for(unsigned int i = 0; i < argc; i = i + 1 )
     {
-        if(startsWith(argv[i])){
+        if(startsWith(argv[i]))
+        {
             cout << argv[i] << endl;
-            argm[argv[i]] = "xxx";
+            char *nextArg = argv[i + 1];
+
+            if(i < argc - 1) 
+            {
+                value = startsWith(argv[i + 1]) ? "" : argv[i + 1];
+            }
+            argm[argv[i]] = value;
         }
     }
 
