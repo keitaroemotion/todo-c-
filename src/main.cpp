a9aa374db30fd71f99f49d3d0aa340311fa63a40
Kei Moomin Sugano <tobasojyo@gmail.com>
2019-10-11 15:37:57 +0900

    command line arguments accepted

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
     cout << "\n\nHello World,\nWelcome to my first C ++ program on Ubuntu Linux\n\n" << endl;
     return 0;
